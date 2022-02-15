90f24765086018afe2f63d55510830f7a1588c1f
John Ward III <Ojos9@aol.com>
2022-02-15 14:45:20 -0500

    Initial commit

---
+++ src/TemperatureMeasurements.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+//Write a class that will collect temperatures, then calculate and the average and mean temperatures
+
+public class TemperatureMeasurements {
+    public void temperatureSensor(){
+        int celsuius = 0;
+        int fareinheit = 0;
+        String date = "";
+    }
+    public void averageTemp(arrayList<>) extends temperatureSensor{
+        int temperatures = 0;
+        int averageTemp = 0;
+        averageTemp = ((ArrayList<temperatures>)*2)/ArrayList.size());
+    }
+    //Write a method to collect/record the temperatures
+    //Create an inherited method to calculate the average temperature with the return of the averageTemp
+    //Create an inherited method to calculate the mean temperature with the return of the meanTemp
+}
